d74ccad367e6ee1e9cb9e6cc4fd63adc3ed0500d
hagi torres <hagiraitorresmacedo@gmail.com>
2022-06-22 17:08:25 -0500

    terminado ejercicio

---
+++ src/main/java/org/ejercicioOne/Main.java
@@ -0,0 +1,19 @@
+package org.ejercicioOne;
+
+public class Main {
+    public static void main(String[] args) {
+        char letra = 'l';
+        String palabra = "palabra";
+        int numeroEntero = 1;
+        long nuemroEntero = 2;
+        double decimal = 1.2;
+        boolean logica = true;
+
+
+        System.out.println("letra"+" "+ letra);
+        System.out.println("palabra"+" "+ palabra);
+        System.out.println("numeroEntero"+" "+ numeroEntero);
+        System.out.println("nuemroEntero"+" "+ nuemroEntero);
+        System.out.println("logica"+" "+ logica);
+    }
+}
